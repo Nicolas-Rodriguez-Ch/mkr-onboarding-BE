11e065aba84268e00ace0399d5e91db2c6f68a63
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-02-16 15:41:26 -0500

    finished end points

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ const table = require("@makeitrealcamp/db-mock")
 const cors = require("cors")
 
 const app = express();
+app.use(express.json())
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    const r1 = table.insert({ task: "Pedro Perez" }) // { id: 1, name: "Pedro Perez" }
-    const r2 = table.insert({ task: "Maria Gomez" }) // { id: 2, name: "Maria Gomez" }
+app.get('/api/tasks', (req, res) => {
     const records = table.findAll();
     res.status(200).json(records);
 })
+
+app.post('/api/tasks', (req, res) =>{
+    const { task } = req.body;
+    if(!task) return res.status(400).json({"error":"You must pass a task"})
+    const r1 = table.insert({ task: task });
+    res.status(202).json({'message':'Recieved succesfully'});
+});
+
+app.put('/api/tasks/:id', (req, res) =>{
+    const { id } = req.params;
+    const { task } = req.body
+    table.update({id: id, task: task});
+    res.status(202).json({'message': 'updated succesfully'})
+});
+
+app.delete('/api/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    table.remove(id);
+    res.status(202).json({'message': 'Deleted succesfully'});
+});
 
 app.listen(8000, ()=> console.log('listening on port 8000'));
