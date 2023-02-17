e5fd71060290ccd2a449dfcd2c3a455746093906
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-02-17 09:15:29 -0500

    removes logs and adds desctiptions

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ app.use(express.json());
 
 app.use(cors());
 
+
+// Trae todos los elementos de la base de datos
 app.get("/api/tasks", (req, res) => {
   const records = table.findAll();
   res.status(200).json(records);
 });
 
+// Crea un nuevo elemento en la BD con lo que trae del body
 app.post("/api/tasks", (req, res) => {
   const { task } = req.body;
   if (!task) return res.status(400).json({ error: "You must pass a task" });
@@ -19,6 +22,7 @@ app.post("/api/tasks", (req, res) => {
   res.status(202).json({ message: "Recieved succesfully" });
 });
 
+//Cambia unatarea individual por params
 app.put("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
   const { task } = req.body;
@@ -26,22 +30,20 @@ app.put("/api/tasks/:id", (req, res) => {
   res.status(202).json({ message: "updated succesfully" });
 });
 
+// Marca como complatada una tarea
 app.put("/api/tasks", (req, res) => {
   const { id, task, check } = req.body;
-
-  console.log(`se recibe ${check}`);
-
   if (check) {
     table.update({ id: id, task: task, check: false });
-    console.log(`se envia ${false}`);
     return res.status(202).json({ message: "task completed" });
   } else {
     table.update({ id: id, task: task, check: true });
-    console.log(`se envia ${true}`);
     return res.status(202).json({ message: "task completed" });
   }
 });
 
+
+//Borra una tarea de la BD
 app.delete("/api/tasks/:id", (req, res) => {
   const { id } = req.params;
   table.remove(id);
